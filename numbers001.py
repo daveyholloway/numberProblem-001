99f6edb3be673dd5fa4aa2f30e166a8247a81a05
W10PANCAKE\davey <davey.holloway@gmail.com>
2017-02-04 00:23:44 +0000

    First attempt, probably can be simplified.

--- numbers001.py
+++ numbers001.py
@@ -1,5 +1,14 @@
 # Seamus Number Puzzle
 #
+# 
+# Choose any 3 digit number (where each digit is different) e.g. 382 
+# Write the digits biggest to smallest and smallest to biggest I.e. 832  and 238
+# Subtract the smaller number from the bigger I.e. 832 - 238
+# When you have your new number, reverse its digits and add these two numbers
+# together I.e. 594 + 495
+# 
+# The number usually arrives at the answer 1089
+#
 # Modification History
 # ====================
 #
@@ -23,10 +32,65 @@ def getRandomNumber():
         if not (myNum in numList):
             numList.append(myNum)
 
-    return((numList[0]*100)+(numList[1]*10)+numList[2])
+    return(toNum(numList))
 
+# Convert a number to a list
+# Assume a 3 digit number
+def toList(pNum):
+    wNumList = []
+    wNumList.append(pNum//100)
+    wNumList.append((pNum - wNumList[0]*100)//10)
+    wNumList.append((pNum - wNumList[0]*100 - wNumList[1]*10))
+
+    return(wNumList)
+
+# Convert a list to a number
+# Assume a 3 digit number
+def toNum(pNumList):
+    return ((pNumList[0]*100) + (pNumList[1]*10) + pNumList[2])
+
+# Sort
+def sortNumFwd(pNum):
+    return(toNum(sorted(toList(pNum))))
+
+# Sort in reverse
+def sortNumRev(pNum):
+    return(toNum(sorted(toList(pNum), reverse=True )))
+
+# Reverse a number
+def reverse(pNum):
+    wList = toList(pNum)
+    wNum  = wList[0]
+    wList[0] = wList[2]
+    wList[2] = wNum
+    return(toNum(wList))
 
 # Let's start
 # Print a few random numbers just to check the generator
+
 for i in range(1,10):
-    print(getRandomNumber())
+    print('===================================================')
+    myNumber = getRandomNumber()
+
+    print('Starting with: ' + str(myNumber))
+
+    print('Step 1 - Sort')
+    print(' Sorted numbers: ' + str(sortNumFwd(myNumber)) + ", " + str(sortNumRev(myNumber)))
+
+    print('Step 2 - Big minus Small')
+    bigMinusSmall = sortNumRev(myNumber) - sortNumFwd(myNumber)
+    print(' Answer: ' + str(bigMinusSmall))
+
+    print('Step 3 - Reverse the answer')
+    print(' Answer: ' + str(reverse(bigMinusSmall)))
+    
+    print('Step 4 - Add together')
+    print(' Answer: ' + str(bigMinusSmall + reverse(bigMinusSmall)))
+
+    if bigMinusSmall + reverse(bigMinusSmall) == 1089:
+        print('Result: *** PASSED ***')
+    else:
+        print('Result: *** FAILED ***')
+
+    print
+    print
